d0b85cf5b51e3f49ff6caba917b344dc462981f7
zidanali111 <zidanali111@gmail.com>
2024-10-14 01:58:15 +0530

    backend final endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono';
-import { use } from 'hono/jsx';
-import { sign } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
 // Create the main Hono app
 const app = new Hono<{
@@ -12,68 +10,9 @@ const app = new Hono<{
   }
 }>();
 
+app.route("/api/v1/user",userRouter);
+app.route("/api/v1/blog",blogRouter);
 
-app.post('/api/v1/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password
-      }
-    });
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
-  } catch (e) {
-    c.status(403);
-    return c.json({ error: "error while signing up" });
-  }
-})
-
-
-app.post('/api/v1/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-      password: body.password
-    }
-  });
-
-  if (!user) {
-    c.status(403);
-    return c.json({ error: "User not found" });
-  }
-
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({
-    message: "User signin successfully",
-    jwt: jwt
-  });
-})
-
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
 
 
 export default app 
